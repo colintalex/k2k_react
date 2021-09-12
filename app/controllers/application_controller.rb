281ec51f0f5b085b3502136a857f29f063d064e7
Colin Alexander <colintalex@gmail.com>
2021-09-12 04:45:47 -0600

    Adds M/C from k2k

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+    helper_method :current_user, :current_admin?
+
+    def current_user
+        @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    end
+
+    def current_admin?
+        current_user.present? && current_user.role.name == 'super_admin'
+    end
+
+    def authenticate
+        redirect_to admin_login_path, alert: 'Top Secret' if current_user.nil?
+    end
 end
